e6349f85941ee9ce2bba0f99739269965ac15868
Josh Schonstal <josh@schonstal.org>
2022-04-08 18:10:52 -0700

    🎥 Add camera distortion

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -3,19 +3,23 @@ class_name ShakingCamera
 
 onready var _screen_shake: ScreenShake = $ScreenShake
 
+
 func _ready():
   position = get_viewport_rect().size / 2
 
-func _process(_delta):
+func _process(delta):
   if Input.is_action_pressed("right"):
-    lean(-PI / 32)
+    lean(-PI / 64)
   elif Input.is_action_pressed("left"):
-    lean(PI / 32)
+    lean(PI / 64)
   else:
     lean(0, 0.1)
+
     
-func lean(radians, speed := 0.02) -> void:
+func lean(radians, speed := 0.05) -> void:
   rotation = lerp(rotation, radians, speed)
+  zoom.x = lerp(zoom.x, 1 + radians / 2.0, speed * 2)
+  zoom.y = lerp(zoom.y, 1 - radians / 2.0, speed * 2)
 
 func trigger_small_shake() -> void:
   _screen_shake.start(.1, 8, 10, 7)

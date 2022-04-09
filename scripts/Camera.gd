219fa6b87fc95d735aee8d84da12ff6c59ca9bf3
Josh Schonstal <josh@schonstal.org>
2022-04-08 17:24:15 -0700

    🎥 Add camera lean

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -3,14 +3,27 @@ class_name ShakingCamera
 
 onready var _screen_shake: ScreenShake = $ScreenShake
 
+func _ready():
+  position = get_viewport_rect().size / 2
+
+func _process(_delta):
+  if Input.is_action_pressed("right"):
+    lean(-PI / 32)
+  elif Input.is_action_pressed("left"):
+    lean(PI / 32)
+  else:
+    lean(0, 0.1)
+    
+func lean(radians, speed := 0.02) -> void:
+  rotation = lerp(rotation, radians, speed)
 
 func trigger_small_shake() -> void:
-	_screen_shake.start(.1, 8, 10, 7)
+  _screen_shake.start(.1, 8, 10, 7)
 
 
 func trigger_medium_shake() -> void:
-	_screen_shake.start(.1, 15, 20, 5)
+  _screen_shake.start(.1, 15, 20, 5)
 
 
 func trigger_large_shake() -> void:
-	_screen_shake.start(.1, 20, 50, 10)
+  _screen_shake.start(.1, 20, 50, 10)

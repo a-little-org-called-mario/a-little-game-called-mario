0a95fcbf8ab6d6fc71611d7430c869a3c36b060c
jotakob <jotakob@users.noreply.github.com>
2022-04-09 21:33:01 +0100

    Adding localization & credits txt
    Adding localization to the project, adding localized RichTextLevels, moving credits to txt file and loading from f ile

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -55,3 +55,7 @@ func _finish_level(next_level : PackedScene = null) -> void:
 func _get_player_spawn_position() -> Vector2:
 	var spawn_points = get_tree().get_nodes_in_group(SPAWNPOINTS_GROUP)
 	return spawn_points[0].global_position if len(spawn_points) > 0 else player.global_position
+
+func _input(event):
+	if event.is_action_pressed("show_credits"):
+		get_tree().change_scene("res://scenes/Credits.tscn")

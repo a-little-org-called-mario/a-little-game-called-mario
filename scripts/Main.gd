3a71601f9fae784a8388a58f86f10b67bbbf49bf
Paul Joannon <hello@pauljoannon.com>
2022-04-10 00:34:12 +0200

    Hub

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -4,6 +4,7 @@ const SPAWNPOINTS_GROUP : String = "SpawnPoints"
 const ENDPORTALS_GROUP : String = "EndPortals"
 const COINS_GROUP : String = "Coins"
 
+onready var hub : TileMap = $TileMap
 onready var level : TileMap = $TileMap
 onready var player : Player = $Player
 
@@ -15,20 +16,19 @@ var coinSound = preload("res://sfx/coin.wav")
 
 func _ready() -> void:
 	EventBus.connect("coin_collected", self, "_on_coin_collected", []);
-	EventBus.connect("build_block", self, "_on_build")
 	_hook_portals()
 	EventBus.connect("crt_filter_toggle",self,"_on_crt_toggle");
 	VisualServer.set_default_clear_color(Color.black)
 
 func _hook_portals() -> void:
-	for portal in get_tree().get_nodes_in_group(ENDPORTALS_GROUP):
-		# Avoid objects that should not be in the group.
-		if not portal is EndPortal:
-			continue
-		# Avoid connecting the same object several times.
-		if portal.is_connected("body_entered", self, "_on_endportal_body_entered"):
-			continue
-		portal.connect("body_entered", self, "_on_endportal_body_entered", [ portal.next_level, portal ])
+    for portal in get_tree().get_nodes_in_group(ENDPORTALS_GROUP):
+        # Avoid objects that should not be in the group.
+        if not portal is EndPortal:
+            continue
+        # Avoid connecting the same object several times.
+        if portal.is_connected("body_entered", self, "_on_endportal_body_entered"):
+            continue
+        portal.connect("body_entered", self, "_on_endportal_body_entered", [ portal.next_level, portal ])
 
 func _on_build() -> void:
 	# If there is a child named TileMap, place a block.
@@ -52,32 +52,32 @@ func _on_build() -> void:
 		
 
 func _on_endportal_body_entered(body : Node2D, next_level : PackedScene, portal) -> void:
-	var animation = portal.on_portal_enter()
-	body.visible = false;
-	yield(animation, "animation_finished");
-	body.visible = true;
-	call_deferred("_finish_level", next_level)
+    var animation = portal.on_portal_enter()
+    body.visible = false;
+    yield(animation, "animation_finished");
+    body.visible = true;
+    call_deferred("_finish_level", next_level)
 
 func _finish_level(next_level : PackedScene = null) -> void:
-	if next_level:
-		# Create the new level, insert it into the tree and remove the old one.
-		var new_level : TileMap = next_level.instance()
-		add_child_below_node(level, new_level)
-		remove_child(level)
-		level = new_level
+    if next_level:
+        # Create the new level, insert it into the tree and remove the old one.
+        var new_level : TileMap = next_level.instance()
+        add_child_below_node(level, new_level)
+        remove_child(level)
+        level = new_level
 
-		# Do not forget to hook the new portals
-		_hook_portals()
-	
-		#Removing instructions 
-		$UI/UI/RichTextLabel.visible = false;
+        # Do not forget to hook the new portals
+        _hook_portals()
+    
+        #Removing instructions 
+        $UI/UI/RichTextLabel.visible = false;
 
-		# We need to flash the player out and in the tree to avoid physics errors.
-		remove_child(player)
-		add_child_below_node(level, player)
-		player.global_position = _get_player_spawn_position()
-		player.look_right()
-		EventBus.emit_signal("level_started", {})
+        # We need to flash the player out and in the tree to avoid physics errors.
+        remove_child(player)
+        add_child_below_node(level, player)
+        player.global_position = _get_player_spawn_position()
+        player.look_right()
+        EventBus.emit_signal("level_started", {})
 
 func _get_player_spawn_position() -> Vector2:
 	var spawn_points = get_tree().get_nodes_in_group(SPAWNPOINTS_GROUP)

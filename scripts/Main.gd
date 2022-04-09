4a8c40b2db18d1ffa2af0f6efbff178c72a3327f
Ahmet Fırat Usta <67150276+KazaKazan@users.noreply.github.com>
2022-04-09 12:43:05 +0300

    Fix mixed indentation errors in Main.gd

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -11,14 +11,14 @@ func _ready() -> void:
   VisualServer.set_default_clear_color(Color(0,0,0,1.0))
 
 func _hook_portals() -> void:
-  for portal in get_tree().get_nodes_in_group(ENDPORTALS_GROUP):
-    # Avoid objects that should not be in the group.
-    if not portal is EndPortal:
-      continue
-    # Avoid connecting the same object several times.
-    if portal.is_connected("body_entered", self, "_on_endportal_body_entered"):
-      continue
-    portal.connect("body_entered", self, "_on_endportal_body_entered", [ portal.next_level, portal ])
+	for portal in get_tree().get_nodes_in_group(ENDPORTALS_GROUP):
+	# Avoid objects that should not be in the group.
+		if not portal is EndPortal:
+		  continue
+		# Avoid connecting the same object several times.
+		if portal.is_connected("body_entered", self, "_on_endportal_body_entered"):
+		  continue
+		portal.connect("body_entered", self, "_on_endportal_body_entered", [ portal.next_level, portal ])
 
 
 func _on_endportal_body_entered(_body : Node2D, next_level : PackedScene, portal) -> void:
@@ -30,20 +30,20 @@ func _on_endportal_body_entered(_body : Node2D, next_level : PackedScene, portal
 
 
 func _finish_level(next_level : PackedScene = null) -> void:
-  if next_level:
-    # Create the new level, insert it into the tree and remove the old one.
-    var new_level : TileMap = next_level.instance()
-    add_child_below_node(level, new_level)
-    remove_child(level)
-    level = new_level
+	if next_level:
+		# Create the new level, insert it into the tree and remove the old one.
+		var new_level : TileMap = next_level.instance()
+		add_child_below_node(level, new_level)
+		remove_child(level)
+		level = new_level
 
-    # Do not forget to hook the new portals
-    _hook_portals()
-    
-    #Removing instructions 
-    $UI/UI/RichTextLabel.visible = false;
+		# Do not forget to hook the new portals
+		_hook_portals()
+		
+		#Removing instructions 
+		$UI/UI/RichTextLabel.visible = false;
 
-    # We need to flash the player out and in the tree to avoid physics errors.
-    remove_child(player)
-    add_child_below_node(level, player)
-    EventBus.emit_signal("level_started", {})
+		# We need to flash the player out and in the tree to avoid physics errors.
+		remove_child(player)
+		add_child_below_node(level, player)
+		EventBus.emit_signal("level_started", {})

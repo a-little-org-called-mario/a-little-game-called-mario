e12198963b53d557613d26b4f8509f790b1361a9
Josh Schonstal <josh@schonstal.org>
2022-04-10 16:26:50 -0700

    🔧 Add GitHub API integration

---
+++ autoload/GitHubAPI.gd
@@ -0,0 +1,23 @@
+# Fetches contributor info from github.
+extends Node
+
+var contributors:Array = []
+
+signal contributors_loaded(names)
+
+func _ready():
+  var http_request = HTTPRequest.new()
+  add_child(http_request)
+  http_request.connect("request_completed", self, "_on_request_completed")
+  http_request.request("https://api.github.com/repos/a-little-org-called-mario/a-little-game-called-mario/contributors")
+  yield(http_request, "request_completed")
+  http_request.queue_free()
+
+func _on_request_completed(_result, _response_code, _headers, body):
+  var json = JSON.parse(body.get_string_from_utf8())
+  contributors = json.result
+  var names:PoolStringArray = []
+  for contributor in contributors:
+    names.append(contributor["login"])
+    
+  emit_signal("contributors_loaded", names)

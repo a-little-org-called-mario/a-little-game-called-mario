d9b71abae88b97497c675cf6915c2f50dbde5da8
Alex McGivern <alex@aeonofdiscord.com>
2022-04-10 20:52:52 +0100

    Fix for Platforms falling out of the sky

--- scripts/Platform.gd
+++ scripts/Platform.gd
@@ -30,6 +30,7 @@ func move(_delta: float):
 	_motion.x = clamp(_motion.x, -max_speed, max_speed)
 	_motion.x = direction * max_speed
 	_motion = move_and_slide(_motion, UP)
+	_motion.y = 0
 
 	_ray.cast_to.x = -1 * direction * RAY_CAST_DISTANCE
 	_ray.force_raycast_update()

81878a9cbfcf15925af38c47a3c41f8e4967d6e9
jam <90727957+JAMSP00N@users.noreply.github.com>
2022-04-10 07:36:52 -0500

    Add screen shake toggle to graphics settings (#114)
    * move shader from scene resource to file for easier toggling
    
    * added basic graphics settings menu to pause screen
    
    * reset menu after exiting back to game
    
    * mapped [p] to pause input
    
    so that the game can still be paused while in fullscreen
    
    * fix left button on crt toggle
    
    i am but a buffoon
    
    * added screen shake gfx setting

--- autoload/Settings.gd
+++ autoload/Settings.gd
@@ -4,4 +4,5 @@ const CameraLeanAmount = preload("res://scripts/CameraLeanAmount.gd")
 
 # graphics settings
 var camera_lean : int = CameraLeanAmount.MAX;
+var screen_shake : bool = true;
 var crt_filter : bool = true;

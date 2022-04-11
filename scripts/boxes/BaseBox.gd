fff6ca6c87c962a935396364231d1d252a8eaf43
DoctorC0X <jonshaft@web.de>
2022-04-11 16:49:26 +0200

    Added CatBox (#146)

--- scripts/boxes/BaseBox.gd
+++ scripts/boxes/BaseBox.gd
@@ -1,6 +1,7 @@
 # Offers basic functionality of a box (animation, counter, ...)
 # Extend it and override the 'on_bounce' func to give it a unique behavior.
 extends Node2D
+class_name BaseBox
 
 const bounce_offset = Vector2(0, -12)
 const bounce_duration = 0.1
@@ -12,6 +13,7 @@ onready var hitArea = $HitArea
 onready var mainCollider : PhysicsBody2D = $Body
 onready var sprite = $Sprite
 onready var tween = $Tween
+onready var audio_meow = $MeowStream
 
 func _ready():
 	hitArea.connect("body_entered", self, "_on_box_entered")
@@ -20,20 +22,21 @@ func _ready():
 
 func _on_box_entered(body):
 	if body is KinematicBody2D and body.position.y > hitArea.global_position.y:
-		call_deferred("bounce")
+		call_deferred("bounce", body)
 
 
 func _physics_process(_delta : float) -> void:
 	(mainCollider.get_child(0) as CollisionShape2D).disabled = !sprite.visible
 
 
-func bounce():
+func bounce(body: KinematicBody2D):
 	sprite.visible = true
 	tween.interpolate_property(sprite, "position", sprite.position, bounce_offset, bounce_duration / 2)
 	tween.start()
 	
 	yield(tween, "tween_all_completed")
-	on_bounce()
+	audio_meow.play()
+	on_bounce(body)
 	
 	tween.interpolate_property(sprite, "position", sprite.position, Vector2.ZERO, bounce_duration / 2)
 	tween.start()
@@ -44,7 +47,7 @@ func bounce():
 		
 
 # override in children to extend the box behavior
-func on_bounce():
+func on_bounce(_body: KinematicBody2D):
 	pass
 
 func disable():

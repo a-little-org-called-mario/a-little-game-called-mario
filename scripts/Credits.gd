7b35c1620227ad7b23e6881c7ac22898d9cafcb4
izzy kestrel <36773942+iznaut@users.noreply.github.com>
2022-04-12 16:10:33 -0500

    Remove Credits.txt and add placeholder credits (for editor testing) (#228)

--- scripts/Credits.gd
+++ scripts/Credits.gd
@@ -7,7 +7,20 @@ onready var vscroll = scroll_container.get_v_scrollbar()
 
 
 func _ready():
-	var label: Label = $ScrollContainer/VBoxContainer/Names
+	var label : Label = $ScrollContainer/VBoxContainer/Names
+
+	# Credits file is generated at build time - use placeholder string if in editor
+	if OS.has_feature("editor"):
+		var sample_names : Array = ["Mario Mario", "Luigi Mario", "Baby Mario"]
+		var names_text : String = ""
+
+		for n in 100:
+			for i in range(0, sample_names.size()):
+				names_text += sample_names[i] + "\n"
+
+		label.text = names_text
+		return
+
 	var file = File.new()
 	file.open("res://credits.txt", File.READ)
 	label.text = file.get_as_text()

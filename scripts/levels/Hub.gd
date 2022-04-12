e3dd02bd8d341f8e9d7f580a8f870525458a00be
Patrick Riordan <patrickriordan177@gmail.com>
2022-04-11 20:06:37 -0700

    Undo accidental whitespace changes to 2 files

--- scripts/levels/Hub.gd
+++ scripts/levels/Hub.gd
@@ -13,61 +13,61 @@ export(PackedScene) var portal_scene : PackedScene
 
 
 func _ready() -> void:
-	var copy_zone := _copy_zone(COPY_ZONE_START, COPY_ZONE_END)
-	var wall_zone := _copy_zone(WALL_START, WALL_END)
+    var copy_zone := _copy_zone(COPY_ZONE_START, COPY_ZONE_END)
+    var wall_zone := _copy_zone(WALL_START, WALL_END)
 
-	var levels := _get_all_first_levels_in_dir(levels_directory)
-	var shift := 0
-	for level in levels:
-		_paste_zone(copy_zone, Vector2(COPY_ZONE_START.x + shift, COPY_ZONE_START.y))
-		var portal : EndPortal = portal_scene.instance()
-		portal.global_position = map_to_world(Vector2(PORTAL_POSITION.x + shift, PORTAL_POSITION.y)) + (cell_size / 2)
-		portal.next_level = load(level)
-		add_child(portal)
-		shift += COPY_ZONE_END.x - COPY_ZONE_START.x + 1
-	_paste_zone(wall_zone, Vector2(COPY_ZONE_START.x + shift, COPY_ZONE_START.y))
+    var levels := _get_all_first_levels_in_dir(levels_directory)
+    var shift := 0
+    for level in levels:
+        _paste_zone(copy_zone, Vector2(COPY_ZONE_START.x + shift, COPY_ZONE_START.y))
+        var portal : EndPortal = portal_scene.instance()
+        portal.global_position = map_to_world(Vector2(PORTAL_POSITION.x + shift, PORTAL_POSITION.y)) + (cell_size / 2)
+        portal.next_level = load(level)
+        add_child(portal)
+        shift += COPY_ZONE_END.x - COPY_ZONE_START.x + 1
+    _paste_zone(wall_zone, Vector2(COPY_ZONE_START.x + shift, COPY_ZONE_START.y))
 
 
 func _copy_zone(topleft : Vector2, bottomright : Vector2) -> Array:
-	var cells := []
-	for x in range(topleft.x, bottomright.x + 1):
-		var column := []
-		for y in range(topleft.y, bottomright.y + 1):
-			column.append(get_cell(x, y))
-		cells.append(column)
-	return cells
+    var cells := []
+    for x in range(topleft.x, bottomright.x + 1):
+        var column := []
+        for y in range(topleft.y, bottomright.y + 1):
+            column.append(get_cell(x, y))
+        cells.append(column)
+    return cells
 
 
 func _paste_zone(zone : Array, topleft : Vector2) -> void:
-	for x in range(0, len(zone)):
-		for y in range(0, len(zone[0])):
-			set_cell(topleft.x + x, topleft.y + y, zone[x][y])
+    for x in range(0, len(zone)):
+        for y in range(0, len(zone[0])):
+            set_cell(topleft.x + x, topleft.y + y, zone[x][y])
 
 
 static func _get_all_first_levels_in_dir(path : String) -> Array:
-	var levels := []
-	var dir := Directory.new()
-	if dir.open(path) == OK:
-		dir.list_dir_begin()
-		var filename := dir.get_next()
-		while filename != "":
-			if filename != "." and filename != ".." and dir.current_is_dir():
-				var level := _get_first_level_in_dir("%s/%s" % [path, filename])
-				if len(level) > 0:
-					levels.append(level)
-			filename = dir.get_next()
-		dir.list_dir_end()
-	return levels
+    var levels := []
+    var dir := Directory.new()
+    if dir.open(path) == OK:
+        dir.list_dir_begin()
+        var filename := dir.get_next()
+        while filename != "":
+            if filename != "." and filename != ".." and dir.current_is_dir():
+                var level := _get_first_level_in_dir("%s/%s" % [path, filename])
+                if len(level) > 0:
+                    levels.append(level)
+            filename = dir.get_next()
+        dir.list_dir_end()
+    return levels
 
 
 static func _get_first_level_in_dir(path : String) -> String:
-	var dir := Directory.new()
-	if dir.open(path) == OK:
-		dir.list_dir_begin()
-		var filename := dir.get_next()
-		while filename != "":
-			if filename != "." and filename != ".." and !dir.current_is_dir():
-				return "%s/%s" % [path, filename]
-			filename = dir.get_next()
-		dir.list_dir_end()
-	return ""
+    var dir := Directory.new()
+    if dir.open(path) == OK:
+        dir.list_dir_begin()
+        var filename := dir.get_next()
+        while filename != "":
+            if filename != "." and filename != ".." and !dir.current_is_dir():
+                return "%s/%s" % [path, filename]
+            filename = dir.get_next()
+        dir.list_dir_end()
+    return ""

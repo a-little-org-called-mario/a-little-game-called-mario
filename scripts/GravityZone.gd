ffa42b79d50feffcf391b1675993622b95509591
Paul Joannon <437025+paulloz@users.noreply.github.com>
2022-04-12 22:56:32 +0200

    Gravity inversion zones are easier to use when level designing (#215)

--- scripts/GravityZone.gd
+++ scripts/GravityZone.gd
@@ -1,4 +1,8 @@
+tool
 extends Area2D
+
+export(RectangleShape2D) var shape : RectangleShape2D setget _set_shape
+export(bool) var revert_on_exit : bool
 
 var general_gravity = preload("res://scripts/resources/Gravity.tres")
 
@@ -9,13 +13,11 @@ func _ready() -> void:
 	connect("body_entered", self, "_on_body_entered")
 	connect("body_exited", self, "_on_body_exited")
 
+	if shape == null:
+		return
 	var sprite: Sprite = $Sprite
 	var sprite_size: Vector2 = sprite.texture.get_size() / Vector2(sprite.hframes, sprite.vframes)
-	var shape: Shape2D = $CollisionShape2D.shape
-	if shape is RectangleShape2D:
-		sprite.scale = Vector2(
-			shape.extents.x * 2 / sprite_size.x, shape.extents.y * 2 / sprite_size.y
-		)
+	sprite.scale = shape.extents * 2 / sprite_size
 
 
 func _on_body_entered(_body: Node) -> void:
@@ -24,4 +26,11 @@ func _on_body_entered(_body: Node) -> void:
 
 
 func _on_body_exited(_body: Node) -> void:
-	general_gravity.direction = previous_gravity_direction
+	if revert_on_exit and previous_gravity_direction.length_squared() > 0:
+		general_gravity.direction = previous_gravity_direction
+	previous_gravity_direction = Vector2.ZERO
+
+
+func _set_shape(new_shape : RectangleShape2D) -> void:
+	shape = new_shape
+	$CollisionShape2D.shape = shape

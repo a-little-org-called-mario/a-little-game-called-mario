7e42a06c3dd78919ecbf39bd4297d424d4776656
simonmnash <simonmnash@users.noreply.github.com>
2022-04-13 18:53:52 +0000

    Auto-Format GDScript

--- scripts/Credits.gd
+++ scripts/Credits.gd
@@ -35,6 +35,9 @@ func _process(delta):
 		vscroll.value += delta * FAST_SPEED
 	else:
 		vscroll.value += delta * DEFAULT_SPEED
-	
-	if vscroll.value >= vscroll.max_value - scroll_container.rect_size.y || Input.is_action_pressed("pause"):
+
+	if (
+		vscroll.value >= vscroll.max_value - scroll_container.rect_size.y
+		|| Input.is_action_pressed("pause")
+	):
 		EventBus.emit_signal("change_scene", {"scene": "res://scenes/title/TitleScreen.tscn"})

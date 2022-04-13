ed5487f40985a9dd37b962b971fecf25961e15ed
Rémi Verschelde <rverschelde@gmail.com>
2022-04-13 09:58:08 +0200

    Fix infinite loop with missing credits.txt
    Fixes #244.

--- scenes/title/Contributors.gd
+++ scenes/title/Contributors.gd
@@ -3,21 +3,28 @@ extends RichTextLabel
 var lines = []
 
 func _ready():
+	randomize()
+
 	var file = File.new()
-	file.open("res://credits.txt", File.READ)
+	var err = file.open("res://credits.txt", File.READ)
+	if err != OK:
+		print("Couldn't load credits.txt. The file might be missing.")
+		return
 	
-	while not file.eof_reached():
-		lines.append(file.get_line())
+	lines = file.get_as_text().split("\n")
 	
 	file.close()
 	setup()
 
 func setup():
+	if lines.empty():
+		return
 	var contrib_array = rand_values(lines)
 	bbcode_text = "\n[wave amp=100 freq=2]%s\n%s\n%s" % [str(contrib_array[0]).to_upper(),str(contrib_array[1]).to_upper(),str(contrib_array[2]).to_upper()]
 	
 func rand_values(arr):
-	randomize()
+	if arr.size() < 3:
+		return arr
 	var rand_array = []
 	var count = 0
 	while count != 3:
@@ -26,7 +33,7 @@ func rand_values(arr):
 		if not rand_array.has(rand_value):
 			rand_array.append(rand_value)
 			count += 1
-	return(rand_array)
+	return rand_array
 
 func _on_Timer_timeout():
 	setup()

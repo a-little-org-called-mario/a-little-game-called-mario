e7508f8d50be8023166847238767be75bfe7448c
Alex CV <42361534+AlexColinVarela@users.noreply.github.com>
2022-04-12 19:49:28 -0500

    CameraFollow as a Scene, you can follow on any axis, and it follows smoooooothly 🎥  (#235)
    * CameraFollow as scene
    
    added a camerafollow scene, just add it to your scene and camera will follow mario horizontally
    
    * Camera now follows smoothly and you can choose wich axis to follow

--- scenes/levels/CameraFollow.gd
+++ scenes/levels/CameraFollow.gd
@@ -1,11 +1,20 @@
 extends Position2D
 onready var camera_reference: Camera2D  = $"../../Camera"
 onready var player_reference: Player  = $"../Player"
+export var follow_horizontally: bool = true
+export var follow_vertically: bool = false
+export(float,0,1) var follow_speed: float = 0.1
 
 func _process(_delta):
-
+	
 	if(not player_reference):
+		print("not player")
 		player_reference = $"../Player"
+
 	if(camera_reference and player_reference):
-		camera_reference.position.x = player_reference.position.x
+		if(follow_horizontally):
+			camera_reference.position.x = lerp(camera_reference.position.x, player_reference.position.x, follow_speed)
+		if(follow_vertically):
+			camera_reference.position.y = lerp(camera_reference.position.y, player_reference.position.y, follow_speed)
+	
 	pass

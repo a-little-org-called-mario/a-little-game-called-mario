de75e0a5391fd32774f9c5d0fc936a1ae5ebd771
SoepIepeI <robin.hinderiks@icloud.com>
2022-04-13 16:25:16 +0200

    Changed cloud spawn position, Changed cloud size

--- scenes/levels/Flappy/env/env_spawner.gd
+++ scenes/levels/Flappy/env/env_spawner.gd
@@ -9,8 +9,8 @@ func _on_env_spawner_timeout():
 	var cloud = CLOUD.instance()
 	get_parent().add_child(cloud)
 	cloud.position.x = 1200
-	cloud.position.y = rand_range(280, 420)
-	var scale = rand_range(0.2, 4.0)
+	cloud.position.y = rand_range(50, 550)
+	var scale = rand_range(1.0, 4.0)
 	cloud.scale = Vector2(scale, scale)
 	cloud.SPEED = -rand_range(50.0, 220.0)
 	self.wait_time = rand_range(0.2, 4.0)

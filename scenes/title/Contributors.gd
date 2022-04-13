b14669e152087f378d4e6781e89ee485b99f5cf8
godofgrunts <godofgrunts@users.noreply.github.com>
2022-04-13 01:03:04 -0400

    An attempt at a better Title Credit area. (#242)
    * An attempt at a better Title Credit area.
    
    * Fixed name of Credits node.

--- scenes/title/Contributors.gd
+++ scenes/title/Contributors.gd
@@ -1,6 +1,32 @@
-extends Label
+extends RichTextLabel
+
+var lines = []
 
 func _ready():
 	var file = File.new()
 	file.open("res://credits.txt", File.READ)
-	text = file.get_as_text().replace("\n", " ")
+	
+	while not file.eof_reached():
+		lines.append(file.get_line())
+	
+	file.close()
+	setup()
+
+func setup():
+	var contrib_array = rand_values(lines)
+	bbcode_text = "\n[wave amp=100 freq=2]%s\n%s\n%s" % [str(contrib_array[0]).to_upper(),str(contrib_array[1]).to_upper(),str(contrib_array[2]).to_upper()]
+	
+func rand_values(arr):
+	randomize()
+	var rand_array = []
+	var count = 0
+	while count != 3:
+		var num = randi() % (arr.size() - 1)
+		var rand_value = arr[num]
+		if not rand_array.has(rand_value):
+			rand_array.append(rand_value)
+			count += 1
+	return(rand_array)
+
+func _on_Timer_timeout():
+	setup()

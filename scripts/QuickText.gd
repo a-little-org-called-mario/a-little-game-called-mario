53ab411c9f88de74578a75b4ba924e24652feef5
Paul Joannon <437025+paulloz@users.noreply.github.com>
2022-04-13 20:28:01 +0200

    Clean repo structure a tiny bit (step 1) (#256)
    * All default levels are in a single folder
    
    * Move autoload/ under scripts/
    
    * Mote Transition/ and SceneTransition/ under scenes/
    
    * Move DDR/ under scenes/
    
    * Move QuickText and Coin files into scripts/ and scenes/ (respectively)
    
    * Move ui/ under scenes/

--- scripts/QuickText.gd
+++ scripts/QuickText.gd
@@ -1,14 +1,9 @@
 extends Label
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
 export var secondsUntilDisappearing = 5;
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
+
 func _process(delta: float) -> void:
 	if visible:
 		secondsUntilDisappearing -= delta;

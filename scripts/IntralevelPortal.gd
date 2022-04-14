4ace88d8f0f69884bc2d49264f6877bf6e190706
iznaut <iznaut@users.noreply.github.com>
2022-04-14 00:40:52 +0000

    Auto-Format GDScript

--- scripts/IntralevelPortal.gd
+++ scripts/IntralevelPortal.gd
@@ -6,13 +6,16 @@ export var teleport_to: Vector2
 
 var player_scene = load("res://scenes/Player.tscn")
 
+
 func _ready():
 	$Sprite.play()
 	connect("body_entered", self, "_on_portal_enter")
 
+
 func _on_portal_enter(body) -> void:
 	if body is Player:
 		call_deferred("teleport", body)
+
 
 func teleport(player: Player) -> void:
 	$PortalSFX.play()

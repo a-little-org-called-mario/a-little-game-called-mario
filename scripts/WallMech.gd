60d6242379cbae1af2909423b095b7961480029e
GodofGrunts <dergottdergrunten@gmail.com>
2022-04-14 14:01:25 -0400

    Cleaned up test.tscn, added some more exported vars.

--- scripts/WallMech.gd
+++ scripts/WallMech.gd
@@ -1,5 +1,7 @@
 # Using the Enemy class that someone else made.
 # The Bub enemy is using collision layer 4 and mask 2 so this enemy is as well.
+# Unfortunately, most of the worlds are made as layer 1 so we need to mask 1 
+# WallMech so it interacts with most worlds. This should be refactored later.
 # Also reusing the res://scenes/enemies/Bullet.tscn and res://scenes/SimpleExplosion.tscn for now
 extends Enemy
 
@@ -14,24 +16,32 @@ var _motion = Vector2.ZERO
 var _col : KinematicCollision2D
 var muzzles : Array = []
 
+# Exported vars for level designers
+# For reference, Vector2.LEFT = (-1, 0) Vector2.DOWN = (0 ,1)
 export(float) var max_speed = 0.75
 export(Vector2) var shoot_direction = Vector2.LEFT
 export(Vector2) var move_direction = Vector2.DOWN
 export(PackedScene) var bullet_scene
 export(PackedScene) var muzzle_flash_scene
-export(int) var health
+export(int) var health = 3
+export(int) var time_between_shots = 3
+export(int) var pre_shot_delay = 1
+export(int) var move_after_shot_delay = 1
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for child in $Muzzles.get_children():
 		muzzles.append(child)
+	shoot_timer.wait_time = time_between_shots
+	move_timer.wait_time = move_after_shot_delay
+	shoot_delay.wait_time = pre_shot_delay
 	shoot_timer.start()
 
-
+# This is function from Enemy.gd that we're overwriting
 func ai(_delta: float) -> void:
 	if _col:
 		move_direction = move_direction * Vector2(-1,-1)
 
+# This is function from Enemy.gd that we're overwriting
 func move(_delta: float) -> void:
 	if _moving:
 		_motion = move_direction * max_speed
@@ -42,10 +52,12 @@ func shoot() -> void:
 	for muzzle in muzzles:
 		var bullet = bullet_scene.instance()
 		var flash = muzzle_flash_scene.instance()
+		# Add the bullets to the root scene so they don't move with the WallMech
 		get_parent().add_child(bullet)
 		muzzle.add_child(flash)
 		bullet.global_position = muzzle.global_position
 		flash.global_position = muzzle.global_position
+		# start_moving is a function from Bullet.tscn
 		bullet.start_moving(shoot_direction)
 		
 	# _moving will be set to true after sound effect plays
@@ -56,9 +68,13 @@ func shoot() -> void:
 # genearl signal, but this is how it is currently done.
 # Here I am overwriting the Enemy.gd's kill function since this unit
 # has health and therefore acts differently.
-func kill(killer : Object) -> void:
+# PlayerProjectile.gd does NOT supply a damage value, however
+# I added a damage var here in case other powerups will eventually
+# It is set to 1 here so the game doesn't complain if the projectile has no
+# damage var.
+func kill(killer : Object, damage : int = 1) -> void:
 	# WallMech has 3 hidden hit points.
-	_damage(1, killer)
+	_damage(damage, killer)
 
 func _damage(dmg : int, killer : Object) -> void:
 	anim.play("hurt")
@@ -72,9 +88,6 @@ func _damage(dmg : int, killer : Object) -> void:
 		alive = false
 		emit_signal("dead", killer)
 		queue_free()
-
-func _handle_dying(_killer):
-	pass
 
 func _on_ShootTimer_timeout() -> void:
 	# WallMech stops to indicate to the player that it is about to attack

a719521b7b131d5d850b059b0014303f4a730a98
Adam Nash <adam@smr.llc>
2022-04-14 01:23:21 -0400

    add talking bunny cat to secret room (#282)

---
+++ scenes/ui/scripts/TextTrigger.gd
@@ -0,0 +1,25 @@
+tool
+extends Area2D
+export var text = "some text" setget set_text, get_text 
+
+func _ready():
+	$TextPanel.visible = false
+	self.connect("body_entered", self, "_on_body_entered")
+	self.connect("body_exited", self, "_on_body_exited")
+
+func _on_body_entered(_body):
+	if not _body.is_in_group("Player"):
+		return
+	$TextPanel.visible = true
+
+func _on_body_exited(_body):
+	if not _body.is_in_group("Player"):
+		return
+	$TextPanel.visible = false
+
+func set_text(t):
+	text = t
+	$TextPanel/Panel/Label.text = tr(text)
+
+func get_text():
+	return text

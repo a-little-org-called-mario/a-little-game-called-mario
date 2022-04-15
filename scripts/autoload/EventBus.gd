7ab4f436a096d00070a4486bdc66d311e15381e0
Paul Joannon <437025+paulloz@users.noreply.github.com>
2022-04-15 06:37:32 +0200

    Implement some big SMW-like buttons (#319)

--- scripts/autoload/EventBus.gd
+++ scripts/autoload/EventBus.gd
@@ -16,6 +16,11 @@ signal player_died
 # Scene Transitions, expects "scene" key
 signal change_scene(data)
 
+# screenshakes
+signal small_screen_shake
+signal medium_screen_shake
+signal large_screen_shake
+
 # pauses the game - used by PauseMenu.gd
 signal game_paused(data)
 # called once on game ends (or is restarted)
@@ -34,3 +39,5 @@ signal fire_flower_collected(data)
 signal bus_collected(data)
 
 signal player_spotted(spotted_by, player)
+
+signal big_button_pressed(button_id)

355f3671bae8209e3d7ceecb9eb89a191b585935
Archy <59789660+IceCryptonym@users.noreply.github.com>
2022-04-15 21:22:00 +1000

    Change _process to _unhandled_input

--- scenes/title/Mario.gd
+++ scenes/title/Mario.gd
@@ -1,10 +1,7 @@
 extends Sprite
 
-#func _ready():
-# pass
 
-
-func _process(delta):
-	if Input.is_action_just_pressed("click"):
+func _unhandled_input(event):
+	if event.is_action_pressed("click"):
 		if get_rect().has_point(to_local(get_global_mouse_position())):
 			$Meow.play()

9ed91c96750817673afbae283cc303dc9ca5d797
Archy <59789660+IceCryptonym@users.noreply.github.com>
2022-04-15 14:38:44 +1000

    Adds Mario to the title screen

--- scenes/title/Mario.gd
+++ scenes/title/Mario.gd
@@ -1,9 +1,10 @@
 extends Sprite
 
-func _ready():
-  position.x = get_viewport().size.x / 2
+#func _ready():
+# pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#  pass
+func _process(delta):
+	if Input.is_action_just_pressed("click"):
+		if get_rect().has_point(to_local(get_global_mouse_position())):
+			$Meow.play()

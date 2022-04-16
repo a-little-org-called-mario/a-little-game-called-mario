f902bc72ff55b3576a8b2b7603a7b582fe0135cd
Simon Nash <simon.m.nash@gmail.com>
2022-04-16 11:14:03 -0400

    Add tab keybinding that shows/hides HUB controls. (#335)

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -75,6 +75,11 @@ func _on_build(data) -> void:
 			# If the cell has a block in in, break the block.
 			level.set_cell(target_tile_x, target_tile_y, 0)
 
+func _input(event):
+	if event.is_action_pressed("show_instructions"):
+		$UI/UI/RichTextLabel.visible = true
+	elif event.is_action_released("show_instructions"):
+		$UI/UI/RichTextLabel.visible = false
 
 func _on_endportal_body_entered(body: Node2D, next_level: PackedScene, portal: EndPortal) -> void:
 	# Make sure the player can't trigger this function more than once.

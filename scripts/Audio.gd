f8cc1a9dba62f4650e27940bf218fce74c6a50b1
monochromia <monochromia@users.noreply.github.com>
2022-04-16 20:19:20 +0000

    Auto-Format GDScript

--- scripts/Audio.gd
+++ scripts/Audio.gd
@@ -1,5 +1,6 @@
 extends Node
 
+
 func _ready():
 	EventBus.connect("volume_changed", self, "_on_volume_change")
 	for bus in ["Master", "music", "sfx", "voice"]:
@@ -7,10 +8,14 @@ func _ready():
 
 
 func _on_volume_change(bus: String):
-	var volume : int = 0
+	var volume: int = 0
 	match bus:
-		"Master": volume = Settings.volume_game
-		"music": volume = Settings.volume_music
-		"sfx": volume = Settings.volume_sfx
-		"voice": volume = Settings.volume_voice
+		"Master":
+			volume = Settings.volume_game
+		"music":
+			volume = Settings.volume_music
+		"sfx":
+			volume = Settings.volume_sfx
+		"voice":
+			volume = Settings.volume_voice
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus), linear2db(volume / 10.0))

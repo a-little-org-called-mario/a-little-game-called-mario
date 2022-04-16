4867fd17c2e873241c993e40162de20b02d0b23e
Luminight <balazscsabai98@gmail.com>
2022-04-16 16:54:15 +0200

    Add scoring system to DDR, fix scene change issues, Mute music/hide ui (#350)

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -7,6 +7,8 @@ const PROJECTILES_GROUP: String = "Projectiles"
 
 onready var hub: TileMap = $TileMap
 onready var level: Node = $TileMap
+onready var bgm: AudioStreamPlayer = $Audio/BGM
+onready var ui: CanvasLayer = $UI
 
 var completionSound = preload("res://audio/sfx/portal.wav")
 var coinSound = preload("res://audio/sfx/coin.wav")
@@ -16,6 +18,8 @@ var entering_portal: bool = false
 
 func _ready() -> void:
 	EventBus.connect("build_block", self, "_on_build")
+	EventBus.connect("bgm_changed", self, "_bgm_changed")
+	EventBus.connect("ui_visibility_changed", self, "_on_ui_visibility_changed")
 	_hook_portals()
 	VisualServer.set_default_clear_color(Color.black)
 
@@ -23,6 +27,14 @@ func _ready() -> void:
 func _exit_tree() -> void:
 	EventBus.emit_signal("game_exit")
 
+func _on_ui_visibility_changed(data):
+	ui.get_child(0).visible = data.visible
+
+func _bgm_changed(data) -> void:
+	if "playing" in data:
+		bgm.playing = data.playing
+	if "stream" in data:
+		bgm.stream = data.stream
 
 func _hook_portals() -> void:
 	for portal in get_tree().get_nodes_in_group(ENDPORTALS_GROUP):

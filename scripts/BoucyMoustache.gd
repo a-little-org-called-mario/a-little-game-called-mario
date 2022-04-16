4bb6ce41ee3d4c54ea8bf31c5363db0d7d4173b9
Ronan Harrington <ronan_harr@protonmail.com>
2022-04-16 23:03:28 +0100

    Fixed C++ unused variable error, changing p to _p

--- scripts/BoucyMoustache.gd
+++ scripts/BoucyMoustache.gd
@@ -13,13 +13,13 @@ onready var leftSegment = $DampedSpringJoint2D2
 func _process(_delta):
 	# line1.global_position = Vector2.ZERO
 	line1.global_rotation = rightSegment.rotation
-	for p in range(len(line1.points)):
+	for _p in range(len(line1.points)):
 		line1.remove_point(0)
 	line1.add_point(line1.position)
 	line1.add_point(segment2.position)
 
 	line2.global_rotation = leftSegment.rotation
-	for p in range(len(line2.points)):
+	for _p in range(len(line2.points)):
 		line2.remove_point(0)
 	line2.add_point(line2.position)
 	line2.add_point(segment22.position)

b4524d04bbdfd3fb2e3bbd56c6b90826c5802cbd
IceCryptonym <59789660+IceCryptonym@users.noreply.github.com>
2022-04-18 15:07:06 +0000

    Adds Mario to the title screen (#323)
    * Adds Mario to the title screen
    
    * Change _process to _unhandled_input
    
    * Fix Mario meow

--- scenes/title/Mario.gd
+++ scenes/title/Mario.gd
@@ -1,9 +1,7 @@
 extends Sprite
 
-func _ready():
-  position.x = get_viewport().size.x / 2
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#  pass
+func _unhandled_input(event):
+	if event.is_action_pressed("click"):
+		if get_rect().has_point(to_local(get_global_mouse_position())):
+			$Meow.play()

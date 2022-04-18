f4101a6a5f095f2cafbbc786eda982ac490be738
Paul Joannon <hello@pauljoannon.com>
2022-04-18 11:11:31 +0200

    Fix crash when collecting stars

--- scenes/ui/StarNotification.gd
+++ scenes/ui/StarNotification.gd
@@ -6,17 +6,13 @@ const VisibleTime = 2
 func _ready():
 	self.hide()
 	EventBus.connect("star_collected", self, "_on_star_collected")
-	EventBus.connect("star_collected_again", self, "_on_star_collected_again")
 
-func _on_star_collected(data: Dictionary):
+
+func _on_star_collected(starname: String, again: bool) -> void:
 	self.show()
-	bbcode_text = "\n" + (tr("[wave amp=50 freq=2]YOU GOT THE STAR:\n[rainbow freq=0.5 sat=1 val=20]%s[/rainbow] [/wave]") % data["name"])
-	yield(get_tree().create_timer(VisibleTime), "timeout")
-	self.hide()
-
-
-func _on_star_collected_again(data: Dictionary):
-	self.show()
-	bbcode_text = "\n" + (tr("[wave amp=50 freq=2]YOU GOT THE STAR:\n[rainbow freq=0.5 sat=1 val=20]%s[/rainbow] [/wave]AGAIN") % data["name"])
+	if not again:
+		bbcode_text = "\n" + (tr("[wave amp=50 freq=2]YOU GOT THE STAR:\n[rainbow freq=0.5 sat=1 val=20]%s[/rainbow] [/wave]") % starname)
+	else:
+		bbcode_text = "\n" + (tr("[wave amp=50 freq=2]YOU GOT THE STAR:\n[rainbow freq=0.5 sat=1 val=20]%s[/rainbow] [/wave]AGAIN") % starname)
 	yield(get_tree().create_timer(VisibleTime), "timeout")
 	self.hide()

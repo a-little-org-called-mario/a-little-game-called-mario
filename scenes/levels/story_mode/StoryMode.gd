d0d5c5eb966e724289447a589b21395206710a63
Jummit <jummit@web.de>
2022-04-19 22:55:53 +0200

    More work on dialogs (#419)
    * More work on dialogs
    
    Dialogs are now loaded by the DialogUI class.
    Add a condition that checks how many times the player has seen a dialog
    option.
    
    * Add lower case characters to font
    
    * More dialog work
    
    * Rework conditions, dialog checking and more
    
    * Remove wiki docstring
    
    * Fix moved dependency
    
    * Overhaul dialog and item loading
    
    Json dialogs are imported as resources so it works on export.
    
    * Actually enable the importer plugin
    
    * Apply default theme
    
    * Fix skip button needing to be pressed twice
    
    * Fix stone dialog

--- scenes/levels/story_mode/StoryMode.gd
+++ scenes/levels/story_mode/StoryMode.gd
@@ -1,46 +1,64 @@
 extends Node2D
 
-var item_store := ItemStore.new()
+"""
+Gamemode in which the story is what matters the most.
 
-const ItemStore = preload("res://scenes/levels/story_mode/ItemStore.gd")
-const Dialog = preload("dialog/Dialog.gd")
+This script handles starting of dialogs by talking to characters.
+"""
+
+var _item_store := ItemStore.new()
+var _door_cell : Vector2
+
+const ItemStore = preload("items/ItemStore.gd")
+const Dialog = preload("res://addons/dialog_importer/dialog.gd")
 const Character = preload("character/Character.gd")
-const FileUtils = preload("res://scripts/FileUtils.gd")
+const DialogUI = preload("dialog/DialogUI.gd")
+const Inventory = preload("items/Inventory.gd")
+const TopDownPlayer = preload("player/TopDownPlayer.gd")
+const ItemReceivePopup = preload("items/ItemReceivePopup.gd")
 
-onready var player: KinematicBody2D = $TopDownPlayer
-onready var dialog_ui: Control = $CanvasLayer/DialogUI
-onready var inventory: ColorRect = $CanvasLayer/Inventory
-onready var item_receive_popup: AcceptDialog = $CanvasLayer/ItemReceivePopup
+onready var _player: TopDownPlayer = $TopDownPlayer
+onready var _dialog_ui: DialogUI = $CanvasLayer/UI/DialogUI
+onready var _inventory: Inventory = $CanvasLayer/UI/Inventory
+onready var _item_receive_popup: ItemReceivePopup = $CanvasLayer/UI/ItemReceivePopup
+onready var _tile_map: TileMap = $TileMap
+onready var _door_position: Position2D = $DoorPosition
+
 
 func _ready() -> void:
+	_door_cell = _tile_map.world_to_map(_door_position.position)
 	for character in $Characters.get_children():
 		character.connect("talked_to", self, "_on_Character_talked_to",
 				[character])
-	dialog_ui.set_items(item_store)
+	_dialog_ui.init(_item_store, _inventory)
+	_dialog_ui.start("intro")
+
+
+func is_door_open():
+	return _tile_map.get_cellv(_door_cell) == 1
 
 
 func _on_Character_talked_to(character: Character) -> void:
-	if dialog_ui.active:
+	if _dialog_ui.active:
 		return
-	player.can_move = false
-	var file = get_script().resource_path.get_base_dir().plus_file("dialogs").plus_file(character.dialog + ".json")
-	var data := FileUtils.as_json(file)
-	if not data:
-		push_error("Couldn't load dialog of character %s: %s" % [character.title, file])
-	dialog_ui.start(character, Dialog.new(data))
+	_dialog_ui.start(character.dialog, character)
 
 
 func _on_DialogUI_dialog_finished() -> void:
-	player.can_move = true
+	_player.can_move = true
 
 
 func _on_DialogUI_item_received(item_id: String) -> void:
-	var item: StoryItem = item_store.get(item_id)
-	item_receive_popup.show_for(item)
-	yield(item_receive_popup, "confirmed")
-	inventory.give(item)
+	var item: StoryItem = _item_store.get(item_id)
+	_item_receive_popup.show_for(item)
+	yield(_item_receive_popup, "confirmed")
+	_inventory.give(item)
 
 
 func _on_DialogUI_event_occured(event) -> void:
 	if event == "open_door":
-		$TileMap.set_cell(20, -11, 1)
+		_tile_map.set_cellv(_door_cell, 1)
+
+
+func _on_DialogUI_dialog_started() -> void:
+	_player.can_move = false
--- scenes/levels/story_mode/dialog/DialogUI.gd
+++ scenes/levels/story_mode/dialog/DialogUI.gd
@@ -1,39 +1,76 @@
 extends Control
 
+"""
+Dialog for displaying interactions with characters.
+"""
+
+signal dialog_started
 signal dialog_finished
+# Emitted when the player receives an item during a dialog.
 signal item_received(item)
+# Emitted when a dialog emits an event. The event is a string.
+# The event is then handled by the main script.
 signal event_occured(id)
 
-export var inventory_path: NodePath
-
+# True if the player is currently in a dialog.
 var active := false
 
+var _dialogs: Dictionary
+var _inventory: Inventory
 var _item_store: ItemStore
 var _dialog: Dialog
 var _speaker: Character
 var _current_text: int
+# A dictionary storing how often a certain dialog has been reached.
+var _occurences: Dictionary
+# The name of the dialog that is being checked for issues.
+var _checking: String
 
-const Dialog = preload("Dialog.gd")
+const Dialog = preload("res://addons/dialog_importer/dialog.gd")
+const Condition = preload("res://addons/dialog_importer/condition.gd")
+const Choice = preload("res://addons/dialog_importer/choice.gd")
 const Character = preload("../character/Character.gd")
-const ItemStore = preload("res://scenes/levels/story_mode/ItemStore.gd")
+const ItemStore = preload("../items/ItemStore.gd")
+const FileUtils = preload("res://scripts/FileUtils.gd")
+const Inventory = preload("../items/Inventory.gd")
 
-onready var _inventory := get_node(inventory_path)
-onready var _dialog_text_label: RichTextLabel = $VBoxContainer/ColorRect/DialogTextLabel
-onready var _continue_button: Button = $VBoxContainer/ColorRect/DialogTextLabel/ContinueButton
+onready var _dialog_text_label: RichTextLabel = $ColorRect/MarginContainer/VBoxContainer/DialogTextLabel
+onready var _continue_button: Button = $ColorRect/MarginContainer/VBoxContainer/DialogTextLabel/ContinueButton
+onready var _finish_button: Button = $ColorRect/MarginContainer/VBoxContainer/DialogTextLabel/FinishButton
 onready var _title_label: Label = $PortraitRect/VBoxContainer/TitleLabel
 onready var _portrait_texture_rect: TextureRect = $PortraitRect/VBoxContainer/PortraitTextureRect
-onready var _choice_container: VBoxContainer = $VBoxContainer/PanelContainer/ChoiceContainer
+onready var _portrait_rect: ColorRect = $PortraitRect
+onready var _choice_container: VBoxContainer = $ColorRect/MarginContainer/VBoxContainer/PanelContainer/ChoiceContainer
 onready var _animation_player: AnimationPlayer = $AnimationPlayer
 
-func set_items(to):
-	_item_store = to
+
+func init(item_store: ItemStore, inventory: Inventory):
+	_item_store = item_store
+	_inventory = inventory
+	var dialog_dir: String = get_script().resource_path\
+			.get_base_dir().plus_file("../dialogs")
+	for _file in FileUtils.list_dir(dialog_dir):
+		var file: String = _file.replace(".import", "")
+		if not file.ends_with(".json"):
+			continue
+		var dialog = load(file)
+		_checking = file.get_file()
+		_check_dialog(dialog)
+		_dialogs[file.get_file().get_basename()] = dialog
 
 
-func start(speaker: Character, dialog: Dialog):
+# Start a dialog with optionally a character as speaker.
+func start(dialog: String, speaker: Character = null):
+	_assert_ready()
+	assert(dialog in _dialogs, "Dialog %s not found" % dialog)
+	emit_signal("dialog_started")
 	_speaker = speaker
-	_title_label.text = speaker.title
-	_portrait_texture_rect.texture = speaker.get_portrait()
-	_set_dialog(dialog)
+	_title_label.text = ""
+	_portrait_rect.visible = speaker != null
+	if _speaker:
+		_title_label.text = _speaker.title
+		_portrait_texture_rect.texture = _speaker.get_portrait()
+	_set_dialog(_dialogs[dialog])
 	active = true
 
 
@@ -43,40 +80,136 @@ func _set_dialog(to: Dialog):
 	if not _dialog:
 		active = false
 		return emit_signal("dialog_finished")
-	if to.is_condition:
-		var condition = _inventory.has(_item_store.get(to.required_item))
-		return _set_dialog(to.True if condition else to.False)
+	if _dialog.condition:
+		var condition := _condition_true(_dialog.condition, _dialog)
+		if _dialog.has_branches:
+			return _set_dialog(_dialog.True if condition else _dialog.False)
+		if not condition:
+			return _set_dialog(null)
 	for button in _choice_container.get_children():
 		button.queue_free()
 	for choice in _dialog.choices:
 		var button := Button.new()
-		button.text = choice
-		button.connect("pressed", self, "_on_ChoiceButton_pressed",
-				[_dialog.choices[choice]])
+		button.text = choice.text
+		var dialog := _evaluate_dialog(choice.dialog)
+		if choice.dialog and not dialog:
+			continue
+		if choice.condition and not _condition_true(choice.condition, choice):
+			continue
+		button.connect("pressed", self, "_on_ChoiceButton_pressed", [dialog])
 		_choice_container.add_child(button)
 	_current_text = -1
 	_progress_dialog()
 
 
+# Move to the next item inside the current dialog.
 func _progress_dialog():
 	_current_text += 1
 	_dialog_text_label.text = tr(_dialog.text[_current_text])
 	var last : bool = _current_text == _dialog.text.size() - 1
 	_continue_button.visible = not last
 	_choice_container.visible = last
+	_finish_button.visible = _dialog.choices.empty() and last
 	if last:
+		_occurences[_dialog] = _occurences.get(_dialog, 0) + 1
 		if _dialog.item:
 			emit_signal("item_received", _dialog.item)
 		if _dialog.event:
 			emit_signal("event_occured", _dialog.event)
 		if _dialog.new_sprite:
+			assert(_speaker, "Can't change sprite in dialog without speaker.")
 			_speaker.set_sprite(_dialog.new_sprite)
+	_animation_player.stop()
 	_animation_player.play("talking")
 
 
-func _on_ChoiceButton_pressed(choice):
+func _condition_true(condition: Condition, object: Object) -> bool:
+	var checks := []
+	if condition.required_item:
+		var item = _item_store.get(condition.required_item)
+		assert(item, "Couldn't find item %s" % condition.required_item)
+		checks.append(_inventory.has(item))
+	if condition.only_at_occurence:
+		var occurence := condition.only_at_occurence - 1
+		checks.append(_occurences.get(object, 0) == occurence)
+	if condition.custom:
+		assert(not condition.custom or owner.has_method(condition.custom),
+				"Can't find custom condition method '%s' in main script."\
+				% condition.custom)
+		checks.append(owner.call(condition.custom))
+	
+	for check in checks:
+		if check == condition.inverted:
+			return false
+	return true
+
+
+# Returns the dialog that should be displayed when this dialog object is hit.
+func _evaluate_dialog(dialog: Dialog) -> Dialog:
+	if not dialog:
+		return null
+	if dialog.condition:
+		if _condition_true(dialog.condition, dialog):
+			if dialog.True:
+				return dialog.True as Dialog
+		else:
+			return dialog.False as Dialog
+	return dialog
+
+
+# Skip the dialog and return if the dialog needed to be skipped.
+func _skip_dialog() -> bool:
+	if _dialog_text_label.visible_characters\
+			< _dialog_text_label.text.length():
+		_animation_player.seek(100, true)
+		return true
+	return false
+
+
+# Check this dialog and the contained conditions/dialogs for any issues.
+func _check_dialog(dialog: Dialog):
+	if not dialog:
+		return
+	_check_condition(dialog.condition)
+	for branch in [dialog.True, dialog.False]:
+		_check_dialog(branch)
+	for _choice in dialog.choices:
+		var choice: Choice = _choice
+		_check_condition(choice.condition)
+		_check_dialog(choice.dialog)
+
+
+# Check if the condition is valid.
+func _check_condition(condition: Condition):
+	if not condition:
+		return
+	if condition.required_item:
+		_check_assert(_item_store.get(condition.required_item),
+				"Couldn't find item %s", condition.required_item)
+	if condition.custom:
+		_check_assert(owner.has_method(condition.custom),
+				"Can't find custom condition method '%s' in main script.",
+				condition.custom)
+
+
+# An assert that prepends the dialog name to the message.
+func _check_assert(condition, message: String, format):
+	assert(condition, "Dialog '%s': %s" % [_checking, message % format])
+
+
+func _on_ChoiceButton_pressed(choice: Dialog):
 	_set_dialog(choice)
 
 
+func _assert_ready():
+	assert(_item_store and _inventory, "Inventory and item provider not set.")
+
+
 func _on_ContinueButton_pressed() -> void:
-	_progress_dialog()
+	if not _skip_dialog():
+		_progress_dialog()
+
+
+func _on_FinishButton_pressed() -> void:
+	if not _skip_dialog():
+		_set_dialog(null)

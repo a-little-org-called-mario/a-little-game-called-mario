2a64f40dde36017f13d6a86cc5b99023a047f5f9
Jummit <jummit@web.de>
2022-04-22 20:33:44 +0200

    Story Mode groundwork: Dialogs Part 4 (#443)
    * Add CharacterData resource
    
    Add a new resource that stores the character information. It can be
    applied to character instances in the world. Start working on item
    pickups. Resize the lever sprite, separate content and logic into
    different folders.
    
    * Add YSort to story mode
    
    All characters, the player and items now need to have their origin at
    the lowest part of the sprite.
    
    * Add ability to pick up ground items
    
    * Add goto functionality to dialog and fix checks
    
    * Make dialogs progressable using the keyboard
    
    * Allow different characters to speak inside dialogs
    
    * Remove intro
    
    * Keep the speaker if none is specified
    
    * Improve dialog schema descriptions

--- addons/dialog_importer/choice.gd
+++ addons/dialog_importer/choice.gd
@@ -1,10 +1,19 @@
 extends Resource
 
+"""
+A choice presented to the player.
+
+Can trigger another dialog or end the conversation.
+Choices can also have conditions that need to be true
+for them to show up.
+"""
+
 export var text: String
 export var condition: Resource
 export var dialog: Resource
 
 const Condition = preload("res://addons/dialog_importer/condition.gd")
+
 
 func _init(data = "") -> void:
 	if data is String:
--- scenes/levels/story_mode/item/ItemStore.gd
+++ scenes/levels/story_mode/item/ItemStore.gd
@@ -2,8 +2,8 @@ var _items : Dictionary
 
 const FileUtils = preload("res://scripts/FileUtils.gd")
 
-func _init() -> void:
-	for item in FileUtils.list_dir("res://scenes/levels/story_mode/items"):
+func _init(dir: String) -> void:
+	for item in FileUtils.list_dir(dir):
 		if item.ends_with(".tres"):
 			_items[item.get_file().get_basename()] = load(item.replace(".import", ""))
 

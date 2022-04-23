40b1a8d43215786118887d7188120e8a3803567a
Alex <No>
2022-04-23 15:53:42 +0200

    Fixed animation retrigger

--- scripts/powerups/Magpie.gd
+++ scripts/powerups/Magpie.gd
@@ -4,16 +4,19 @@ onready var pieSprite: AnimatedSprite = $PieSprite
 onready var explosionSprite: AnimatedSprite = $ExplosionSprite
 
 func _on_body_entered(body: Node2D):
-	pieSprite.playing = false
-	pieSprite.visible = false
-	explosionSprite.visible = true
-	explosionSprite.play("expansion")
-
 	HeartInventoryHandle.change_hearts_on(body, -3)
 
 	if body is Player:
 		var dir = body.global_position - global_position
 		body.bounce(8000, dir)
+	
+	if explosionSprite.is_playing():
+		return
+	
+	pieSprite.playing = false
+	pieSprite.visible = false
+	explosionSprite.visible = true
+	explosionSprite.play("expansion")
 	
 	yield(explosionSprite, "animation_finished")
 	# the expand & explode have different FPS, hence different animations

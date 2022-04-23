0922f6844b5f603635c22cd8c19a2488ee5fc3ac
jamspoon <jamspoonart@gmail.com>
2022-04-22 19:31:03 -0500

    add aiming

--- scripts/marwing/Marwing.gd
+++ scripts/marwing/Marwing.gd
@@ -1,12 +1,17 @@
 extends MarwingShip
 
+export var crosshair_range : Vector2;
+export var crosshair_speed: float;
+var crosshair_pos: Vector2 = Vector2.ZERO;
+
+onready var camera: Camera = get_node("Camera");
+onready var mesh: MeshInstance = get_node("Mesh");
+onready var crosshair: Sprite3D = get_node("Crosshair");
+
 func _ready ():
-	$Camera.set_current(true);
+	camera.set_current(true);
 
 func _physics_process (dt: float):
-	#note (jam): it would be really neat if this ended up being an exponential rather than linear acceleration
-	#			 but i'm not in the mood to do that now
-
 	# handle strafing based on player inputs
 	if can_strafe:
 		# horizontal strafing
@@ -17,7 +22,7 @@ func _physics_process (dt: float):
 		if 0 != perpen.x and 0 == Input.get_action_strength("marwing_left") and 0 == Input.get_action_strength("marwing_right"):
 			perpen.x = perpen.x + (dt * strafe_speed * (-0.75 if 0 < perpen.x else 0.75));
 			if abs(perpen.x) < (dt * strafe_speed * 0.75): perpen.x = 0;
-		$Mesh.translation.x = perpen.x;		# apply offset to mesh
+		mesh.translation.x = perpen.x;		# apply offset to mesh
 
 		# vertical strafing
 		perpen.y = clamp(
@@ -26,12 +31,23 @@ func _physics_process (dt: float):
 		if 0 != perpen.y and 0 == Input.get_action_strength("marwing_up") and 0 == Input.get_action_strength("marwing_down"):
 			perpen.y = perpen.y + (dt * strafe_speed * (-0.75 if 0 < perpen.y else 0.75));
 			if abs(perpen.y) < (dt * strafe_speed * 0.75): perpen.y = 0;
-		$Mesh.translation.y = perpen.y;
+		mesh.translation.y = perpen.y;
 
 		# apply rotation to emphasize strafing
-		if 0 != perpen.x: $Mesh.rotation_degrees.z = max_lean.x * (-perpen.x / max_perpen.x);
-		if 0 != perpen.y: $Mesh.rotation_degrees.x = max_lean.y * (perpen.y / max_perpen.y);
+		if 0 != perpen.x: mesh.rotation_degrees.z = max_lean.x * (-perpen.x / max_perpen.x);
+		if 0 != perpen.y: mesh.rotation_degrees.x = max_lean.y * (perpen.y / max_perpen.y);
 
+	# handle aiming
+	if can_shoot:
+		crosshair_pos.x = clamp(
+			crosshair_pos.x - delta_aim("marwing_aimright", dt) + delta_aim("marwing_aimleft", dt),
+			-crosshair_range.x, crosshair_range.x);
+		crosshair.translation.x = crosshair_pos.x;
+
+		crosshair_pos.y = clamp(
+			crosshair_pos.y - delta_aim("marwing_aimdown", dt) + delta_aim("marwing_aimup", dt),
+			-crosshair_range.y, crosshair_range.y);
+		crosshair.translation.y = crosshair_pos.y;
 	pass;
 
 ## Calculates how much weight a given input should have on the ship's strafe offset by factoring in strafe speed and input strength.
@@ -40,4 +56,9 @@ func _physics_process (dt: float):
 func delta_strafe (input_name: String,dt: float) -> float:
 	if Input.is_action_pressed(input_name):
 		return dt * strafe_speed * Input.get_action_strength(input_name);
+	return float(0);
+
+func delta_aim (input_name: String,dt: float) -> float:
+	if Input.is_action_pressed(input_name):
+		return dt * crosshair_speed * Input.get_action_strength(input_name);
 	return float(0);

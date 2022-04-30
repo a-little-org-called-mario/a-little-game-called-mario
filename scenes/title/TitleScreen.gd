e89bc586e44036393133ee025e4ffc5104e6ff5f
jamspoon <jamspoonart@gmail.com>
2022-04-30 14:45:39 -0500

    create function for handling useragent info

--- scenes/title/TitleScreen.gd
+++ scenes/title/TitleScreen.gd
@@ -1,4 +1,12 @@
 extends Control
 
 func _ready():
+	check_os_browser()
 	Settings.load_data()
+
+func check_os_browser ():
+	var agent: String = JavaScript.eval("navigator.userAgent")
+	
+	#disable marwing label on mac + chrome
+	if "Mac" in agent and "Chrome" in agent:
+		$VBoxContainer/VBoxContainer/HBoxContainer/MarwingButton.hide()

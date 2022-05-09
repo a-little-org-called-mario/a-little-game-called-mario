7e53c1384505d6da15de330d7c6f95e53041a966
jamspoon <90727957+JAMSP00N@users.noreply.github.com>
2022-05-08 20:51:28 -0500

    ddr: .sm parser (#490)
    * initial implementation
    
    * register relevant classes
    
    almost forgot !
    
    * refactor / better documentation
    
    works just as well, but is more clear and consistently written & documented

---
+++ scenes/DDR/SMChartList.gd
@@ -0,0 +1,11 @@
+class_name SMChartList
+
+var chart_type: String
+var description: String
+var difficulty: String
+var meter: int
+var groove_radar = []
+var left_notes = []
+var right_notes = []
+
+func _init (): pass

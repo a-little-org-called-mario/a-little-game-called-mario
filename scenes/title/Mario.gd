2f30ba16b5c1a9c09d722cbc3843d256e124a9ab
AwesomeCoder412412 <44704454+AwesomeCoder412412@users.noreply.github.com>
2022-06-21 02:14:29 -0700

    Add Woem functionality (#510)

--- scenes/title/Mario.gd
+++ scenes/title/Mario.gd
@@ -4,4 +4,10 @@ extends Sprite
 func _unhandled_input(event):
 	if event.is_action_pressed("click"):
 		if get_rect().has_point(to_local(get_global_mouse_position())):
-			$Meow.play()
+			var rng = RandomNumberGenerator.new()
+			rng.randomize()
+			var num = rng.randi_range(0, 6)
+			if (num == 0):
+				$Woem.play()
+			else:
+				$Meow.play()

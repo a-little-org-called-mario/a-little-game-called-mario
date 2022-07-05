afbe7cde5a435e3c856542975e02cbbd140b2388
captaincolonelfox <lentrog@gmail.com>
2022-07-06 00:48:32 +0300

    Skip deathscreen on jump/click/pause/restart actions

--- scenes/ui/PlayerDied.gd
+++ scenes/ui/PlayerDied.gd
@@ -1,13 +1,22 @@
 extends Panel
 
-const VisibleTime: int = 5
 const FORMAT: String = "[center][wave amp=50 freq=2][rainbow freq=0.5 sat=1 val=20]%s[/rainbow][/wave][/center]"
+const SKIP_ACTIONS: Array = ["jump", "click", "pause", "restart"]
 
 onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _ready():
 	EventBus.connect("player_died", self, "_on_player_died")
+
+
+func _process(_delta: float) -> void:
+	if not self.visible:
+		return
+	for skip_action in SKIP_ACTIONS:
+		if Input.is_action_just_pressed(skip_action):
+			animation_player.emit_signal("animation_finished")
+			break
 
 
 func _on_player_died():

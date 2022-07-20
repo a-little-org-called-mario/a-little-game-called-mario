282185e05bab6dd7dd44d56bc28124c38fb519d1
captaincolonelfox <lentrog@gmail.com>
2022-07-21 01:06:23 +0300

    Remove Flappy Mario from titlescreen
    * Add sky background
    
    * Remove additional UI node
    
    * Add ability to return to hub

--- scenes/levels/Flappy/UI_flappy_gameover.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-# I have no idea if this is used anywhere
-#warning-ignore:UNUSED_CLASS_VARIABLE
-var inventory = preload("res://scripts/resources/PlayerInventory.tres")
-
-func _unhandled_input(event):
-	if event.is_action_pressed("restart"):
-		get_tree().reload_current_scene()

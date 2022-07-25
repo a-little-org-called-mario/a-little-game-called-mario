14c8b52c722d16c2daf2e426795509542369fe93
Woahsea <lucyma0550@gmail.com>
2022-07-25 19:33:56 +0100

    portal

--- scripts/cutscene/GassyExit.gd
+++ scripts/cutscene/GassyExit.gd
@@ -14,6 +14,10 @@ func _on_boss_dead(_attackerName):
 	visible = true
 	_part.emitting = true
 	_b_death.play()
-	_tween.interpolate_property(self, "position:x", position.x, position.x + 3000, 1, Tween.TRANS_BACK, Tween.EASE_IN, 3.5)
+	_tween.interpolate_property(self, "position:x", position.x, position.x + 3000, 1, Tween.TRANS_BACK, Tween.EASE_IN, 2.5)
 	_tween.start()
+
+
+func _on_tween_completed(_object, _key):
+	EventBus.emit_signal("show_all_portals")
 

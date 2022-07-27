b1c324b1d8e5c9adc3c7c63d7b7a3f23aaeee139
Woahsea <lucyma0550@gmail.com>
2022-07-27 12:19:03 +0100

    adding new note sprites instructions

--- scripts/objects/NotesNewTrigger.gd
+++ scripts/objects/NotesNewTrigger.gd
@@ -3,6 +3,11 @@ extends Area2D
 
 export (String) var pageName = "blank"
 export (String, MULTILINE) var pageDesc = "blank blank more blank blank blank change this etc"
+
+# This the name of the animation to be displayed by the AnimatedSprite.
+# Set this to one currently available there, or add a new one.
+# You can check what's already there or add a new animation by going to
+# res://scenes/ui/Notes.tscn and going to the node called "Sprite".
 export var pageSprite = "little_mario"
 
 

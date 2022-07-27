5e7e0e5951ba49ce52b35d0cf3804a0aa80d4d49
Woahsea <lucyma0550@gmail.com>
2022-07-27 16:27:17 +0100

    remove reset_focus()

--- scripts/ui/Notes.gd
+++ scripts/ui/Notes.gd
@@ -57,20 +57,6 @@ func add_button(name):
 	button.text = name
 	button.connect("page_changed", self, "_on_page_changed")
 	_list.add_child(button)
-	nextPageID += 1
-
-
-func reset_focus():
-	# reseting the focus path for the buttons
-	var buttons = []
-	for node in _list.get_children():
-		buttons.append(node)
-	
-	for i in len(buttons):
-		if i != 0:
-			buttons[i].focus_neighbour_top = buttons[i-1].get_path()
-		if i != len(buttons)-1:
-			buttons[i].focus_neighbour_bottom = buttons[i+1].get_path()
 
 
 func _on_note_added(name, desc, sprite, spriteScale):
@@ -83,7 +69,6 @@ func _on_note_added(name, desc, sprite, spriteScale):
 	# add a new page
 	if not found:
 		add_page(name, desc, sprite, spriteScale)
-		reset_focus()
 
 
 func _on_page_changed(pageName):

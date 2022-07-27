3be96e6378cdb2cde018957687388fb4a4949f78
Woahsea <lucyma0550@gmail.com>
2022-07-27 18:14:53 +0100

    improved notes exit options

--- scripts/ui/Notes.gd
+++ scripts/ui/Notes.gd
@@ -10,26 +10,31 @@ onready var _sprite := $Hbox/VBox/Panel/Sprite
 onready var _name := $Hbox/VBox/Center/Name
 onready var _desc := $Hbox/VBox/Center2/Description
 onready var _hbox := $Hbox
+onready var _exit := $Exit
 
 
 func _ready():
 	$Hbox.visible = false
+	$Exit.visible = false
 	EventBus.connect("note_added", self, "_on_note_added")
 	remove_pages()
 
 
 func _process(delta):
-	if Input.is_action_just_pressed("show_notes"):
+	if Input.is_action_just_pressed("show_notes") or (Input.is_action_just_pressed("pause") and _hbox.visible):
 		toggle_visible()
 
 
 func toggle_visible():
 	if _hbox.visible:
 		_hbox.visible = false
+		_exit.visible = false
 		get_tree().paused = false
 	elif get_tree().paused == false:
 		_hbox.visible = true
-		_list.get_child(0).grab_focus()
+		_exit.visible = true
+		if _list.get_child_count() > 0:
+			_list.get_child(0).grab_focus()
 		get_tree().paused = true
 
 
@@ -61,4 +66,8 @@ func _on_page_changed(pageName):
 	_desc.text = pageInfo.desc
 	_sprite.animation = pageInfo.sprite
 	_sprite.scale = pageInfo.scale
+
+
+func _on_Exit_pressed():
+	toggle_visible()
 

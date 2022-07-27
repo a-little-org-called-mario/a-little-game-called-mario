5289aa660c030f23edc3ddfe4b9aadc1985ba0c3
Woahsea <lucyma0550@gmail.com>
2022-07-27 11:08:08 +0100

    NotesNewTrigger always checking fix

--- scripts/objects/NotesNewTrigger.gd
+++ scripts/objects/NotesNewTrigger.gd
@@ -6,10 +6,8 @@ export var pageDesc = "blank blank more blank blank blank change this etc"
 export var pageSprite = "little_mario"
 
 
-func _physics_process(delta):
-	if monitoring:
-		for body in get_overlapping_bodies():
-			if body is Player:
-				EventBus.emit_signal("note_added", pageName, pageDesc, pageSprite)
-				monitoring = false
+func _on_body_entered(body):
+	if body is Player:
+		EventBus.emit_signal("note_added", pageName, pageDesc, pageSprite)
+		set_deferred("monitoring", false)
 

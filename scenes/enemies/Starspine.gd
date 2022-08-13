d7d200ce483ee70a55fdee8e9dac43b4a454aca1
CrazedTinkerer <110867557+CrazedTinkerer@users.noreply.github.com>
2022-08-13 15:21:24 -0500

    Finished Starspine and a new level to use it

--- scenes/enemies/Starspine.gd
+++ scenes/enemies/Starspine.gd
@@ -1,6 +1,10 @@
 extends Enemy
 
 export var speed := 200.0
+export var health := 3
+export var time_until_sleep := 1.0 #How long the Starspine stays active when it can't see the player
+export var hurt_flash_duration := 0.15
+export var hurt_flash_color := Color(1.0, 0.5, 0.5)
 
 enum State {IDLE, WAKE, ACTIVE, SLEEP}
 export(State) var state := State.IDLE
@@ -9,10 +13,34 @@ var targetRelPosition := Vector2(0,0)
 
 onready var raycast := $RayCast2D
 onready var sprite := $Sprite
+onready var sleep_timer := $SleepTimer
+onready var hurt_flash_timer := $HurtFlashTimer
 
 #Hitbox.gd checks this
 # warning-ignore:unused_class_variable
 var damage = 0
+
+
+# warning-ignore:shadowed_variable
+func kill(killer: Object, damage: int = 1) -> void:
+	_damage(damage, killer)
+
+
+func _damage(dmg: int, killer: Object) -> void:
+	sprite.modulate = hurt_flash_color
+	hurt_flash_timer.start(hurt_flash_duration)
+	
+	health = health - dmg
+	if health <= 0:
+		EventBus.emit_signal("enemy_killed")
+		emit_signal("dying", killer)
+		var res = _handle_dying(killer)
+		if res is GDScriptFunctionState:
+			yield(res, "completed")
+		alive = false
+		emit_signal("dead", killer)
+		queue_free()
+
 
 func move(_delta):
 	var playerRelPosition = get_player_pos()
@@ -27,12 +55,19 @@ func move(_delta):
 		State.ACTIVE:
 			if raycast.get_collider() is Player:
 				targetRelPosition = playerRelPosition
+				sleep_timer.stop()
+			else:
+				if sleep_timer.is_stopped():
+					sleep_timer.start(time_until_sleep)
 			move_and_slide(targetRelPosition.normalized() * speed)
 	
 	
 func get_player_pos() -> Vector2:
-	var target : Node2D = get_tree().get_nodes_in_group("Player")[0]
-	return target.global_position - global_position
+	var target_group = get_tree().get_nodes_in_group("Player")
+	if target_group.size() > 0:
+		var target : Node2D = target_group[0]
+		return target.global_position - global_position
+	return Vector2.ZERO
 	
 	
 func wake():
@@ -43,6 +78,7 @@ func wake():
 	
 func sleep():
 	assert(state == State.ACTIVE)
+	sleep_timer.stop()
 	state = State.SLEEP
 	sprite.play("sleep")
 	damage = 0
@@ -58,3 +94,11 @@ func _on_AnimatedSprite_animation_finished():
 		State.SLEEP:
 			state = State.IDLE
 			sprite.play("idle")
+
+
+func _on_SleepTimer_timeout():
+	sleep()
+
+
+func _on_HurtFlashTimer_timeout():
+	sprite.modulate = Color(1.0, 1.0, 1.0)

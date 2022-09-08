aa08832ad5492a4f61a2ddca9eb04f3de4a74479
Harmony Honey <36719038+HarmonyHoney@users.noreply.github.com>
2022-09-07 18:13:02 -0700

    More dialog for Blug! With randomization (: (#640)

--- scripts/friends/Blug.gd
+++ scripts/friends/Blug.gd
@@ -5,11 +5,14 @@ const SPEED = 2
 const DIST = 64
 
 onready var _sprite := $Sprite
+onready var _text_trigger := $TextTrigger
 
+export (String, MULTILINE) var dialog = "blug\nglug\nhello"
+var queue := []
 
 func _ready():
 	$Collision.visible = true
-
+	fill_queue()
 
 func _physics_process(delta):
 	var playPos = Vector2()
@@ -24,3 +27,16 @@ func _physics_process(delta):
 		position.x -= SPEED
 		_sprite.scale.x = -2
 
+func _on_TextTrigger_body_exited(_body):
+	randomize()
+	if randf() > 0.3:
+		if queue.empty():
+			fill_queue()
+		if !queue.empty():
+			_text_trigger.text = queue.pop_back()
+	else:
+		_text_trigger.text = "blug"
+
+func fill_queue():
+	queue = dialog.split("\n")
+	queue.shuffle()

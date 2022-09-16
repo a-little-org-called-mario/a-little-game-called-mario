e7e068e8f4b6c58f65be238d41998bcaff49e589
captaincolonelfox <lentrog@gmail.com>
2022-09-17 00:00:50 +0300

    Fix black screen on Start Game (#642)

--- scenes/ui/scripts/TextTrigger.gd
+++ scenes/ui/scripts/TextTrigger.gd
@@ -1,25 +1,27 @@
-tool
+class_name TextTrigger
 extends Area2D
-export var text = "some text" setget set_text, get_text 
+
+export var text = "some text" setget set_text
+
 
 func _ready():
 	$TextPanel.visible = false
 	self.connect("body_entered", self, "_on_body_entered")
 	self.connect("body_exited", self, "_on_body_exited")
 
+
 func _on_body_entered(_body):
 	if not _body.is_in_group("Player"):
 		return
 	$TextPanel.visible = true
+
 
 func _on_body_exited(_body):
 	if not _body.is_in_group("Player"):
 		return
 	$TextPanel.visible = false
 
+
 func set_text(t):
 	text = t
 	$TextPanel/Panel/Label.text = tr(text)
-
-func get_text():
-	return text

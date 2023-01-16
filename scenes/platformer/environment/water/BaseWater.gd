5029fbebfbf4f245e05895ff829dc8fc138eaca5
CrazedTinkerer <110867557+CrazedTinkerer@users.noreply.github.com>
2023-01-16 17:26:21 -0600

    Add a water level

---
+++ scenes/platformer/environment/water/BaseWater.gd
@@ -0,0 +1,49 @@
+class_name Water
+extends Area2D
+
+#Warning: Although things can be in multiple water areas at the same time, do not allow
+# anything to be in water areas with different multipliers at the same time.
+# Strange things will happen.
+export var gravity_multiplier := 0.2
+export var jump_multiplier := 0.5
+
+const IN_WATER_GROUP := "in_water"
+const WATER_COUNT_META := "water_count"
+
+
+func _physics_process(_delta):
+	var bodies := get_overlapping_bodies()
+	for temp_body in bodies:
+		var body : PhysicsBody2D = temp_body
+		if "double_jump" in body:
+			body.double_jump = true
+
+
+func _on_body_entered(body: Node):
+	#This keeps a count of how many water areas a body is in.
+	#This count is needed so physics work correctly when a body is touching multiple water areas.
+	var old_water_count := 0
+	if body.has_meta(WATER_COUNT_META):
+		old_water_count = body.get_meta(WATER_COUNT_META)
+	body.set_meta(WATER_COUNT_META, old_water_count + 1)
+	body.add_to_group(IN_WATER_GROUP)
+	
+	if old_water_count == 0: #The body was not in water already
+		if "gravity" in body:
+			body.gravity.strength *= gravity_multiplier
+		if "jump_force" in body:
+			body.jump_force *= jump_multiplier
+
+
+func _on_body_exited(body: Node):
+	assert(body.has_meta(WATER_COUNT_META))
+	var old_water_count : int = body.get_meta(WATER_COUNT_META)
+	body.set_meta(WATER_COUNT_META, old_water_count - 1)
+	
+	if old_water_count == 1: #The body just exited the last water area
+		body.remove_from_group(IN_WATER_GROUP)
+		if "gravity" in body:
+			body.gravity.strength /= gravity_multiplier
+		if "jump_force" in body:
+			body.jump_force /= jump_multiplier
+	
---
+++ scenes/platformer/environment/water/WaterBox.gd
@@ -0,0 +1,49 @@
+tool
+extends Water
+
+export var size := Vector2(1024, 300) setget size_set
+export var color := Color(0.0, 0.3, 0.7, 0.5) setget color_set
+export var surface_color := Color(0.8, 0.8, 1.0, 0.7) setget surface_color_set
+export var surface := true setget surface_set
+
+
+var is_ready := false
+
+func _ready():
+	is_ready = true
+	update_size()
+	update_params()
+
+func size_set(value: Vector2):
+	size = value
+	if is_ready: #Make sure child nodes are loaded first
+		update_size()
+		
+func color_set(value):
+	color = value
+	if is_ready: #Make sure child nodes are loaded first
+		update_params()
+	
+func surface_color_set(value):
+	surface_color = value
+	if is_ready: #Make sure child nodes are loaded first
+		update_params()
+		
+func surface_set(value):
+	surface = value
+	if is_ready: #Make sure child nodes are loaded first
+		update_params()
+		
+
+func update_size():
+	$Display.scale = size
+	$Display.position = size
+	$CollisionShape2D.shape.extents = size / 2
+	$CollisionShape2D.position = size / 2
+	$Display.material.set_shader_param("size", size)
+	
+
+func update_params():
+	$Display.material.set_shader_param("color", color)
+	$Display.material.set_shader_param("surface_color", surface_color)
+	$Display.material.set_shader_param("surface", surface)

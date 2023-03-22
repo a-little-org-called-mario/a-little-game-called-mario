cbb8397c6125ab24606e53a466421df74a82385b
radzo73 <radzo73qwerty@gmail.com>
2023-03-22 16:52:39 -0400

    Revert "Palette support for Mario"
    This reverts commit 28b1e22fa11dacae94c46c0b3c89ebb37a0ab7a3.

--- scenes/levels/Mario's Edge/Pause.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func _physics_process(delta):
-	if(get_tree().paused):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	else:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
--- scenes/levels/Mario's Edge/Reticle.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite
-
-func _ready():
-	center()
-
-
-func center():
-	self.position = get_viewport_rect().get_center()

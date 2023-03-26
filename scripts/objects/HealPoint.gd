2459950d8529ea5be5088426d1660b45db99f959
Woahsea <lucyma0550@gmail.com>
2023-03-26 19:13:49 +0100

    use body_entered signal instead

--- scripts/objects/HealPoint.gd
+++ scripts/objects/HealPoint.gd
@@ -1,5 +1,4 @@
 extends Area2D
 
-func _physics_process(delta):
-	for body in get_overlapping_bodies():
-		HeartInventoryHandle.change_hearts_on(body, 99)
+func _body_entered(body):
+	HeartInventoryHandle.change_hearts_on(body, 99)
